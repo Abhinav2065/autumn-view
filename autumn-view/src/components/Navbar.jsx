3540f107fd384c45ffc34253564f173a34d0f44e
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-26 02:59:29 +0545

    First commit!

---
+++ autumn-view/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../style/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Link 1</li>
+                <li>Link 2</li>
+                <li>Link 3</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
