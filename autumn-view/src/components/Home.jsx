bc6bc20b991fa87767c687c25205b1b3b7cee593
Abhinav2065 <abhinavsl511@gmail.com>
2025-10-26 16:24:18 +0545

    Added an Map Preview

--- autumn-view/src/components/Home.jsx
+++ autumn-view/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from './Home/Hero'
 import QuickAction from './Home/QuickAction'
+import MapPreview from './Home/MapPreview'
 import '../style/Home.css'
 
 
@@ -9,6 +10,7 @@ const Home = () => {
     <div className='home'>
         <Hero></Hero>
         <QuickAction></QuickAction>
+        <MapPreview></MapPreview>
     </div>
   )
 }
---
+++ autumn-view/src/components/Home/MapPreview.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+const MapPreview = () => {
+  return (
+    <div>
+        <div className="map-preview">
+            <div className="map-header">
+                <h2>Explore Great Places Near You.</h2>
+                <p>View Full Map!</p>
+            </div>
+
+            <div className="map-container">
+                Map Loading...
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default MapPreview
